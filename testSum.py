d24d5d6b019fc86057694a019c929174506cb874
jobinjoshu0 <53798306+jobinjoshu0@users.noreply.github.com>
2019-08-09 13:36:42 -0700

    Update testSum.py

--- testSum.py
+++ testSum.py
@@ -20,3 +20,20 @@ x = sumTwo (3,5)
 print(x)
 
 print(getNumber())
+
+def sumDigits():
+                while True:
+                        number = int(input("Please enter a number: "))
+                        if number > 0: 
+                                quotient = number % 10
+                                Onestorage = quotient
+                                difference = number - quotient
+                                divide = diffreence//10
+                                twosome = divide % 10 
+                                Twostorage = twosome 
+                                selfo = divide - twosome
+                                final = selfo//10
+                                Threestorage = final 
+                                digitsum = Threestorage + Twostorage + Onestorage
+                                return digitsum
+print(sumDigits())                        
